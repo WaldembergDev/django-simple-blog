e82c34891ba28b59b027e129fe689758399b38b1
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-17 21:02:43 -0300

    Inicio do projeto. Criado o app accounts e posts. Desenvolvido a class CustomUser

---
+++ accounts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, name, surname, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            name = name,
+            surname = surname,
+            email = email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, name, surname, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(name, surname, email, password, **extra_fields)
+        
+
+# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=120)
+    email = models.EmailField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'surname']
