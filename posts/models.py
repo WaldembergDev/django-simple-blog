f6c95f348a626b6cbc51e1a7eedfb4af8091c1ae
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-18 19:26:27 -0300

    Criado as classes Post e Categories

--- posts/models.py
+++ posts/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+from accounts.models import CustomUser
+
+class StatusEnumCategories(models.TextChoices):
+    ACTIVE = 'Ativo'
+    INACTIVE = 'Inativo'
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=8, choices=StatusEnumCategories.choices, default=StatusEnumCategories.ACTIVE)
+
+    def __str__(self):
+        return self.name
+
+class StatusEnumPost(models.TextChoices):
+    SKETCH = 'Rascunho'
+    PUBLISHED = 'Publicado'
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+    created_in = models.DateTimeField()
+    updated_in = models.DateTimeField()
+    slug = models.SlugField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=StatusEnumPost.choices, default=StatusEnumPost.SKETCH)
+    categories = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
