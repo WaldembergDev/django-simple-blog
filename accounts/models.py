c3eb8595e475de18e2a8e5a02d8581c05abdcacd
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-18 13:13:06 -0300

    Desenvolvido o template e view register

--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 from django.utils import timezone
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, name, surname, email, password, **extra_fields):
+    def create_user(self, first_name, surname, email, password, **extra_fields):
         email = self.normalize_email(email)
         user = self.model(
-            name = name,
+            first_name = first_name,
             surname = surname,
             email = email,
             **extra_fields
@@ -15,17 +15,23 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, name, surname, email, password, **extra_fields):
+    def create_superuser(self, first_name, surname, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(name, surname, email, password, **extra_fields)
+        return self.create_user(first_name, surname, email, password, **extra_fields)
+
+class GenderEnum(models.TextChoices):
+    MASCULINE = 'M'
+    FEMININE = 'F'
         
 
 # Create your models here.
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     surname = models.CharField(max_length=120)
     email = models.EmailField(max_length=100, unique=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GenderEnum.choices, blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
@@ -33,4 +39,4 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'surname']
+    REQUIRED_FIELDS = ['first_name', 'surname']
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
 
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        first_name = request.POST.get('first_name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        date_of_birth = request.POST.get('date_of_birth')
+        gender = request.POST.get('gender')
+        user_exist = CustomUser.objects.filter(email = email).exists()
+        if user_exist:
+            return redirect('/account/register/?status=error_1')
+        user = CustomUser.objects.create_user(
+            first_name = first_name,
+            surname = surname,
+            email = email,
+            password = password,
+            date_of_birth = date_of_birth,
+            gender = gender
+        )
+        user.save()
+        return redirect('/account/register/')
