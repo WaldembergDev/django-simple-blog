f86548c6cb35626712203400c39d6b6befcd6060
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-18 21:01:11 -0300

    Desenvolvendo o inicio da tela de criação de post

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+@login_required(login_url='/account/login/')
+def new_post(request):
+    return render(request, 'new_post.html')
 
 
         
