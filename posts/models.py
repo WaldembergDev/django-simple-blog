0dd1078a96c0ee945ee50815fbd015460feebada
waldembergdev <waldemberg.pereira@outlook.com>
2025-05-01 12:53:44 -0300

    Desenvolvido a view new_post

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from accounts.models import CustomUser
+from django.utils.text import slugify
 
 class StatusEnumCategories(models.TextChoices):
     ACTIVE = 'Ativo'
     INACTIVE = 'Inativo'
 
 class Categories(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     status = models.CharField(max_length=8, choices=StatusEnumCategories.choices, default=StatusEnumCategories.ACTIVE)
 
     def __str__(self):
@@ -21,10 +22,16 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
     created_in = models.DateTimeField()
-    updated_in = models.DateTimeField()
+    updated_in = models.DateTimeField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     status = models.CharField(max_length=10, choices=StatusEnumPost.choices, default=StatusEnumPost.SKETCH)
     categories = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # transforma o título em um slug válido
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Categories, Post
+from django.utils import timezone
 
 # Create your views here.
 @login_required(login_url='/account/login/')
 def new_post(request):
-    return render(request, 'new_post.html')
-
-
-        
+    if request.method == 'GET':
+        categories = Categories.objects.all()
+        return render(request, 'new_post.html', {'categories': categories})
+    else:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.user
+        created_in = timezone.now()
+        categorie = request.POST.get('categorie')
+        model_categorie = Categories.objects.filter(name = categorie).first()
+        new_post = Post(
+            title = title,
+            content = content,
+            author = author,
+            created_in = created_in,
+            categories = model_categorie
+        )
+        new_post.save()
+        return redirect('/posts/new_post')
