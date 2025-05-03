f5909c4b38f48bb6f6da98869574c695a1cedf9a
waldembergdev <waldemberg.pereira@outlook.com>
2025-05-03 15:23:38 -0300

    Desenvolvido a view edit_post

--- posts/views.py
+++ posts/views.py
@@ -33,4 +33,25 @@ def delete_post(request, id):
     return redirect('/plataform/home/')
 
 def edit_post(request, id):
-    pass
+    post = Post.objects.filter(id = id).first()
+    categories = Categories.objects.all()
+    if request.method == 'GET':
+        return render(request, 'edit_post.html', {'post': post, 'categories': categories})
+    else:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        categorie = request.POST.get('categorie')
+        update_in = timezone.now()
+        # obtendo a categoria
+        model_categorie = Categories.objects.filter(name = categorie).first()
+        # atualizando o post
+        post.title = title
+        post.content = content
+        post.categories = model_categorie
+        post.updated_in = update_in
+        # salvando as alterações do post
+        post.save()
+        # redirecionando o usuário para a mesma página
+        return redirect('edit_post', id=post.id)
+                
+    
